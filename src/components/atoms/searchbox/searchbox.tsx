09c88195ff81cf0c8c62e341a1b037941f063df8
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-09-25 01:56:19 +0530

    added changes in search box

--- src/components/atoms/searchbox/searchbox.tsx
+++ src/components/atoms/searchbox/searchbox.tsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useClickAway } from 'react-use';
 import styles from './searchbox.module.scss';
+import Link from 'next/link';
 
 const SearchBox = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -22,7 +23,6 @@ const SearchBox = () => {
         if (isOpen) setIsOpen(false);
     });
 
-    // Close overlay on "Escape" key press
     // Close overlay on "Escape" key press
     useEffect(() => {
         const handleKeyDown = (event: KeyboardEvent) => {
@@ -78,9 +78,7 @@ const SearchBox = () => {
                             transition={{ duration: 0.16 }}
                         >
                             <div className={styles.overlayContent} ref={overlayRef}>
-                                <div
-                                    className={styles.searchinput}
-                                >
+                                <div className={styles.searchinput}>
                                     <svg xmlns="http://www.w3.org/2000/svg" className={styles.searchicon} viewBox="0 0 39 39">
                                         <path d="M25.8762 23.8695C27.1206 22.2102 27.8545 20.1538 27.8545 17.9273C27.8545 12.4531 23.4015 8 17.9273 8C12.4531 8 8 12.4531 8 17.9273C8 23.4015 12.4531 27.8545 17.9273 27.8545C20.1538 27.8545 22.2102 27.1171 23.8695 25.8762L28.2694 30.2761C28.5459 30.5526 28.9075 30.6909 29.2727 30.6909C29.6379 30.6909 29.9995 30.5526 30.2761 30.2761C30.8292 29.723 30.8292 28.826 30.2761 28.2694L25.8762 23.8695ZM17.9273 25.0182C14.0166 25.0182 10.8364 21.8379 10.8364 17.9273C10.8364 14.0166 14.0166 10.8364 17.9273 10.8364C21.8379 10.8364 25.0182 14.0166 25.0182 17.9273C25.0182 21.8379 21.8379 25.0182 17.9273 25.0182Z" />
                                     </svg>
@@ -92,34 +90,40 @@ const SearchBox = () => {
                                         placeholder='Search for series, chapters, ...'
                                     />
                                 </div>
+
+                                <div className={styles.bottomlayer}>
+                                    <div className={styles.quicklinks}>
+                                        <Link className={styles.quicklink} href="/">
+                                            trends
+                                        </Link>
+
+                                        <Link className={styles.quicklink} href="/">
+                                            freebies
+                                        </Link>
+
+                                        <Link className={styles.quicklink} href="/">
+                                            modern fonts
+                                        </Link>
+
+                                        <Link className={styles.quicklink} href="/">
+                                            checklist
+                                        </Link>
+                                    </div>
+                                    <div className={styles.searchfooter}>
+                                        <div className={styles.links}>
+                                            <Link className={styles.linkchip} href="/">Terms</Link>
+                                            <Link className={styles.linkchip} href="/">Refunds</Link>
+                                            <Link className={styles.linkchip} href="/">Shipping</Link>
+                                        </div>
+                                    </div>
+                                </div>
+
+
                             </div>
                         </motion.div>
                     </>
                 )}
             </AnimatePresence>
-
-            {/* {isOpen && (
-                <motion.div
-                    className={styles.overlay}
-                    initial={{ opacity: 0, y: '-100%' }}
-                    animate={{ opacity: 1, y: '0%' }}
-                    exit={{ opacity: 0, y: '-100%' }}
-                    // transition={{ duration: 0.2 }}
-                    ref={overlayRef}  // Attach the ref to the overlay
-                >
-
-                    <motion.button
-                        className={styles.searchbox}
-                        whileTap={{ opacity: 0.6 }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.searchicon} viewBox="0 0 39 39">
-                            <path d="M25.8762 23.8695C27.1206 22.2102 27.8545 20.1538 27.8545 17.9273C27.8545 12.4531 23.4015 8 17.9273 8C12.4531 8 8 12.4531 8 17.9273C8 23.4015 12.4531 27.8545 17.9273 27.8545C20.1538 27.8545 22.2102 27.1171 23.8695 25.8762L28.2694 30.2761C28.5459 30.5526 28.9075 30.6909 29.2727 30.6909C29.6379 30.6909 29.9995 30.5526 30.2761 30.2761C30.8292 29.723 30.8292 28.826 30.2761 28.2694L25.8762 23.8695ZM17.9273 25.0182C14.0166 25.0182 10.8364 21.8379 10.8364 17.9273C10.8364 14.0166 14.0166 10.8364 17.9273 10.8364C21.8379 10.8364 25.0182 14.0166 25.0182 17.9273C25.0182 21.8379 21.8379 25.0182 17.9273 25.0182Z" />
-                        </svg>
-                        Search
-                    </motion.button>
-
-                </motion.div>
-            )} */}
         </>
     );
 };
