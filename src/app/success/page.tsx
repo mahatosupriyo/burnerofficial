9e70cb9d9559e4879738d2ff594b8f3873f615f3
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-09-27 03:35:08 +0530

    added changes

---
+++ src/app/success/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './success.module.scss'
+import Logo from '@/components/base/logo'
+import SuccessAnimation from '@/components/atoms/lotties/success'
+
+const SuccessPage = () => {
+    return (
+        <div className={styles.wraper}>
+            <div className={styles.container}>
+                <Logo width='90' />
+
+                <div className={styles.mainwraper}>
+                    <SuccessAnimation />
+                    <h3 className={styles.heading}>
+                        Your payment was successful.
+                    </h3>
+                    <p className={styles.subheading}>
+                        we're processing your payment.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SuccessPage
