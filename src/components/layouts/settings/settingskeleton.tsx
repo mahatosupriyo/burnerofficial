05bb09f7fe12e96e0cd1f01ab73503809e165a4b
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-09-28 23:45:31 +0530

    added changes

---
+++ src/components/layouts/settings/settingskeleton.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './settingskeleton.module.scss'
+
+const SettingsSkeleton = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        padding: '4rem'
+      }}
+    >
+      <div className={styles.settingslayout}>
+        <div className={styles.header}>
+          <div className={styles.avatar}>
+
+          </div>
+          <div className={styles.name}>
+
+          </div>
+        </div>
+        <div className={styles.content}>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SettingsSkeleton
