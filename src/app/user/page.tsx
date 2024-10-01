949aa3980e675b8d69fc33dfb3aaf4e239eb44ac
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-10-01 22:01:51 +0530

    fixed sitemap

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import styles from './user.module.scss'
 import UserProfile from '@/components/layouts/userprofile/userprofile'
 import NavBar from '@/components/navbar/navbar'
-import { Sitemap } from '@/components/atoms/sitemap/sitemap'
 
 const UserPage = () => {
     return (
         <div className={styles.wraper}>
             <NavBar />
             <div className={styles.container}>
-                {/* <UserProfile/> */}
-                <Sitemap />
+                <UserProfile/>
             </div>
         </div>
     )
