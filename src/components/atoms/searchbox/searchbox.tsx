8f6ede6df9fb4b9d04467dcd9dec94a25b3bb297
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-10-02 00:59:41 +0530

    added placeholder

--- src/components/atoms/searchbox/searchbox.tsx
+++ src/components/atoms/searchbox/searchbox.tsx
@@ -144,27 +144,37 @@ const SearchBox: React.FC = () => {
                             transition={{ duration: 0.16 }}
                         >
                             <div className={styles.overlayContent} ref={overlayRef}>
-                                <div className={styles.inputbarwraper}>
-                                    <div className={styles.mainsearch}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.searchicon} viewBox="0 0 39 39">
-                                            <path d="M25.8762 23.8695C27.1206 22.2102 27.8545 20.1538 27.8545 17.9273C27.8545 12.4531 23.4015 8 17.9273 8C12.4531 8 8 12.4531 8 17.9273C8 23.4015 12.4531 27.8545 17.9273 27.8545C20.1538 27.8545 22.2102 27.1171 23.8695 25.8762L28.2694 30.2761C28.5459 30.5526 28.9075 30.6909 29.2727 30.6909C29.6379 30.6909 29.9995 30.5526 30.2761 30.2761C30.8292 29.723 30.8292 28.826 30.2761 28.2694L25.8762 23.8695ZM17.9273 25.0182C14.0166 25.0182 10.8364 21.8379 10.8364 17.9273C10.8364 14.0166 14.0166 10.8364 17.9273 10.8364C21.8379 10.8364 25.0182 14.0166 25.0182 17.9273C25.0182 21.8379 21.8379 25.0182 17.9273 25.0182Z" />
-                                        </svg>
+                                <div className={styles.toplayer}>
 
-                                        <input
-                                            ref={inputRef}
-                                            type="text"
-                                            className={styles.inputcontainer}
-                                            placeholder="Search"
-                                            value={search}
-                                            onChange={(e) => setSearch(e.target.value)}
-                                            onFocus={() => setError(null)}
-                                        />
+                                    <div className={styles.inputbarwraper}>
+                                        <div className={styles.mainsearch}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.searchicon} viewBox="0 0 39 39">
+                                                <path d="M25.8762 23.8695C27.1206 22.2102 27.8545 20.1538 27.8545 17.9273C27.8545 12.4531 23.4015 8 17.9273 8C12.4531 8 8 12.4531 8 17.9273C8 23.4015 12.4531 27.8545 17.9273 27.8545C20.1538 27.8545 22.2102 27.1171 23.8695 25.8762L28.2694 30.2761C28.5459 30.5526 28.9075 30.6909 29.2727 30.6909C29.6379 30.6909 29.9995 30.5526 30.2761 30.2761C30.8292 29.723 30.8292 28.826 30.2761 28.2694L25.8762 23.8695ZM17.9273 25.0182C14.0166 25.0182 10.8364 21.8379 10.8364 17.9273C10.8364 14.0166 14.0166 10.8364 17.9273 10.8364C21.8379 10.8364 25.0182 14.0166 25.0182 17.9273C25.0182 21.8379 21.8379 25.0182 17.9273 25.0182Z" />
+                                            </svg>
+
+                                            <input
+                                                ref={inputRef}
+                                                type="text"
+                                                className={styles.inputcontainer}
+                                                placeholder="Search"
+                                                value={search}
+                                                onChange={(e) => setSearch(e.target.value)}
+                                                onFocus={() => setError(null)}
+                                            />
+                                        </div>
                                     </div>
+                                    <button className={styles.btn} onClick={() => setIsOpen(false)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.closeicon} fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#1a1a1a" >
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+
                                 </div>
+
 
                                 <div className={styles.bottomlayer}>
                                     <div className={styles.drops}>
-                                        {isLoading && <SearchItemSkeleton/>}
+                                        {isLoading && <SearchItemSkeleton />}
                                         {error && <div className={styles.error}>{error}</div>}
                                         {isInitialState && items.length === 0 && (
                                             <div className={styles.empty}>start searching</div>
