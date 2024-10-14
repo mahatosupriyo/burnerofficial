c6ae00fbe376abf7ac139bb67c3892ca532251ba
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-10-15 00:50:39 +0530

    added user changes

--- src/components/molecules/banner/banner.tsx
+++ src/components/molecules/banner/banner.tsx
@@ -6,24 +6,18 @@ import styles from './banner.module.scss'
 const posts = [
     {
         id: 1,
-        title: "Collaboration Secrets",
-        subtitle: "BEHIND THE SCENES",
         image: "https://images.unsplash.com/photo-1727807232404-78c35fd9dd31?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNHx8fGVufDB8fHx8fA%3D%3D",
         pageNumber: "01",
         totalPages: "03",
     },
     {
         id: 2,
-        title: "User Experience: The Art of Intuition",
-        subtitle: "DESIGN PRINCIPLES",
         image: "https://images.unsplash.com/photo-1728356454433-8b11b5c0c917?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzOHx8fGVufDB8fHx8fA%3D%3D",
         pageNumber: "02",
         totalPages: "03",
     },
     {
         id: 3,
-        title: "From Concept to Launch : A Journey",
-        subtitle: "PRODUCT DEVELOPMENT",
         image: "https://plus.unsplash.com/premium_photo-1728566648330-7ba0d675f3c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2N3x8fGVufDB8fHx8fA%3D%3D",
         pageNumber: "03",
         totalPages: "03",
@@ -106,7 +100,7 @@ export default function PostCard() {
                 className={styles.draggablePost}
                 drag="x"
                 dragConstraints={{ left: -100, right: 100 }}
-                dragElastic={0.4}
+                dragElastic={0.1}
                 onDragStart={handleDragStart}
                 onDrag={handleDrag}
                 onDragEnd={handleDragEnd}
