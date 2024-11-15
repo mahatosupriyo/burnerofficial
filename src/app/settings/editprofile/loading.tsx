4e83091c01c9d630500018e2c51101d667aa3c8a
Supriyo Mahato <mesupriyomahato@gmail.com>
2024-11-15 23:53:15 +0530

    addded changes in security

--- src/app/settings/editprofile/loading.tsx
+++ src/app/settings/editprofile/loading.tsx
@@ -1,11 +1,11 @@
-import EditProfileSkeleton from "@/components/layouts/settings/editprofile/editprofileskeleton";
-import NavBar from "@/components/navbar/navbar";
+import BurnerLoader from "@/components/atoms/lotties/loader";
 
 export default function Loading() {
     return (
         <>
-            <NavBar />
-            <EditProfileSkeleton />
+            <div style={{ height: '100lvh', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <BurnerLoader />
+            </div>
         </>
     )
 }
